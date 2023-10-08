c5244fd2cc7ad6dce3b7d6ca6462bb85a7aa91c3
Precious Iwuanyanwu <iwuanyanwuprecious97@gmail.com>
2023-10-08 00:48:38 -0700

    created another 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A ptogram that prints a line to a standard error
+ * Return: 0(success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
