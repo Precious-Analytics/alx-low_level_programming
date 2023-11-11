56e909befd0ffaf111d3a9d67310f12f90e51511
Precious Iwuanyanwu <iwuanyanwuprecious97@gmail.com>
2023-11-11 03:38:25 -0800

    Task on 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,7 +8,10 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("d\n%", argc);
-	printf("s%\n", argv[]);
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
