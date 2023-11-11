007200445ce4d9c73941c03a07046f53c26fbc23
Precious Iwuanyanwu <iwuanyanwuprecious97@gmail.com>
2023-11-11 04:45:21 -0800

    solved task 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "main.h"
 /**
  * main - add two postive numbers
  * @argc: size of arguments
  * @argv: array of arguments
- * Return: 0(success), 1(fail)
+ * Return: (0) success, (1) fail
  */
 int main(int argc, char *argv[])
 {
+	int i, j, sum = 0;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	int sum = 0;
-	for (int i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			printf("Error\n");
 			return (1);
 		}
+		sum += atoi(argv[i]);
 	}
-	sum += atoi(argv[i]);
 	printf("%d\n", sum);
 	return (0);
 }
